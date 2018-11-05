bbac9b8d7b258616a892a2fbf76e56239d5a6b50
DR <danielrubak@gmail.com>
2018-11-05 18:07:01 +0100

    Added some tasks content to Lab02.sql

--- Lab_02/Lab02.sql
+++ Lab_02/Lab02.sql
@@ -12,3 +12,33 @@ SELECT nazwa, ulica, miejscowosc FROM klienci ORDER BY miejscowosc DESC, nazwa A
 -- 4. Wyświetla listę klientów posortowaną malejąco według nazw miejscowości
 
 -- 5. Wyświetla listę klientów z Krakowa posortowaną według nazw klientów.
+
+-- Zadanie 2.2, baza danych: cukiernia
+
+-- 1. Wyświetla nazwę i masę czekoladek, których masa jest większa niż 20 g.
+-- 2. Wyświetla nazwę, masę i koszt produkcji czekoladek, których masa jest większa niż 20 g i koszt produkcji jest większy niż 25 gr.
+-- 3. J.w. ale koszt produkcji musi być podany w groszach.
+-- 4. Wyświetla nazwę oraz rodzaj czekolady, nadzienia i orzechów dla czekoladek, które są w mlecznej czekoladzie i nadziane malinami lub są w mlecznej czekoladzie i nadziane truskawkami lub zawierają orzechy laskowe, ale nie są w gorzkiej czekoladzie.
+-- 5. Wyświetla nazwę i koszt produkcji czekoladek, których koszt produkcji jest większy niż 25 gr.
+-- 6. Wyświetla nazwę i rodzaj czekolady dla czekoladek, które są w białej lub mlecznej czekoladzie.
+
+-- Zadanie 2.3
+
+-- 1. 124 * 7 +45
+-- 2. 2 ^ 20
+-- 3. sqrt(3)
+-- 4. pi
+
+-- Zadanie 2.4, baza danych: cukiernia
+-- 1. Masa mieści się w przedziale od 15 do 24 g.
+-- 2. Koszt produkcji mieści się w przedziale od 25 do 35 gr.
+-- 3. Masa mieści się w przedziale od 25 do 35 g lub koszt produkcji mieści się w przedziale od 15 do 24 gr.
+
+
+-- Zadanie 2.5, baza danych: cukiernia
+
+-- Zadanie 2.6, baza danych: cukiernia
+
+-- Zadanie 2.7, baza danych: cukiernia
+
+-- Zadanie 2.8, baza danych: cukiernia
